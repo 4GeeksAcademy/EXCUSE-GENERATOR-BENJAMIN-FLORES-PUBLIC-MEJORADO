57e3ffb105ad829a89ed87a66e3db433012bd7a6
Benjyflores <113569896+Benjyflores@users.noreply.github.com>
2023-11-21 20:21:22 +0000

    Publico

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let generateExcuse = () => {
+    let pronoun = ["A", "The"];
+    let subject = ["dog", "racoon", "turtle", "bird", "comedian"];
+    let action = ["took my", "threw my", "stole my"];
+    let possetion = ["homework", "toe", "car", "show"];
+    let where = ["on the street", "in my house", "in my driveway"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    let pronounIndex = Math.floor(Math.random() * pronoun.length);
+    let subjectIndex = Math.floor(Math.random() * subject.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let possetionIndex = Math.floor(Math.random() * possetion.length);
+    let whereIndex = Math.floor(Math.random() * where.length);
+
+    return (
+      pronoun[pronounIndex] +
+      " " +
+      subject[subjectIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      possetion[possetionIndex] +
+      " " +
+      where[whereIndex]
+    );
+  };
+  document.querySelector("#BTN").addEventListener("click", () => {
+    //Excusa random
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
 };
